3351e81fe9136246d048fd3e4f581da89a53bbe4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-07 00:41:54 +0100

    build: new ui button

---
+++ src/ui/CustomButton.java
@@ -0,0 +1,27 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomButton extends JButton {
+    private static final String BUTTON_HTML_TEMPLATE = "<html><div style=\"padding: 8px 15px; "
+                                                       + "margin: 5px; border-radius: 10px; background-color: #%s; "
+                                                       + "color: white; font-family: Arial;\">%s</div></html>";
+
+    public CustomButton(String text, String backgroundColor) {
+        super(String.format(BUTTON_HTML_TEMPLATE, backgroundColor, text));
+        initializeButton();
+    }
+
+    public CustomButton(String text) {
+        this(text, "007bff");
+    }
+
+    public void initializeButton() {
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setFocusPainted(false);
+        setOpaque(false);
+    }
+}
