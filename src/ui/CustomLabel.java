183f97d2732f4b6f823a0b8f4317787d12930e68
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-06 20:46:21 +0100

    build: custom label component

---
+++ src/ui/CustomLabel.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+
+public class CustomLabel extends JLabel {
+    public final static String PRE = "<html><p style=\"margin-left: 10px;\">";
+    public final static String POST = "</p></html>";
+
+    public CustomLabel(String text) {
+        super(PRE + text + POST);
+    }
+
+    public CustomLabel(String text, boolean isCompleted) {
+        super(isCompleted ? PRE + "<strike>" + text + "</strike>" + POST : PRE + text + POST);
+    }
+}
