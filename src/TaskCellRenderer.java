26c8c2703be011748c5cc4a1aa4aeac8f8b59cb5
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-06 19:21:28 +0100

    build: upload images for priority

---
+++ src/TaskCellRenderer.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TaskCellRenderer extends JPanel implements ListCellRenderer<Task> {
+    private JLabel nameLabel;
+    private JLabel iconLabel;
+
+    public TaskCellRenderer() {
+        setLayout(new BorderLayout());
+        nameLabel = new JLabel();
+        iconLabel = new JLabel();
+        add(iconLabel, BorderLayout.WEST);
+        add(nameLabel, BorderLayout.CENTER);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Task> list, Task task, int index, boolean isSelected, boolean cellHasFocus) {
+        nameLabel.setText(task.getName());
+        nameLabel.setForeground(task.isCompleted() ? Color.GRAY : Color.BLACK);
+
+        switch (task.getPriority()) {
+            case HIGH -> iconLabel.setIcon(new ImageIcon("red.jpg"));
+            case MEDIUM -> iconLabel.setIcon(new ImageIcon("yellow.jpg"));
+            case LOW -> iconLabel.setIcon(new ImageIcon("green.jpg"));
+        }
+
+        setBackground(isSelected ? Color.LIGHT_GRAY : Color.WHITE);
+        return this;
+    }
+}
+
