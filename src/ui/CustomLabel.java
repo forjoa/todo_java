ffea854591d1a95edbbc6a9e05b47b1448bf477b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-07 01:11:43 +0100

    refactor: update label margins

--- src/ui/CustomLabel.java
+++ src/ui/CustomLabel.java
@@ -3,7 +3,7 @@ package ui;
 import javax.swing.*;
 
 public class CustomLabel extends JLabel {
-    public final static String PRE = "<html><p style=\"margin-left: 10px;\">";
+    public final static String PRE = "<html><p style=\"margin: 0px 10px;\">";
     public final static String POST = "</p></html>";
 
     public CustomLabel(String text) {
